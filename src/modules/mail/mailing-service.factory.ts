6be3843e092b744e3c9cb57b8464b096566152bc
muhammetozen <muhammed_ozenn@hotmail.com>
2023-11-10 20:03:09 +0300

    v1

--- src/modules/mail/mailing-service.factory.ts
+++ src/modules/mail/mailing-service.factory.ts
@@ -4,7 +4,7 @@ import { MailgunService } from './mailing-services/mailgun-service';
 import { SendgridService } from './mailing-services/sendgrid-service';
 
 /**
- * Imeplementing the factory design pattern to follow Liskov substitution principle
+ * Imeplementing the factory design pattern to follow open-close principle
  *
  * This pattern will make switching between different services fairly simple
  */
